9fbecfa55f1cc54b84a5be61217db547a6cec139
Thomas Quinot <thomas@quinot.org>
2021-01-24 00:18:35 +0100

    Build using single job with matrix

--- .github/workflows/build-pkg.sh
+++ .github/workflows/build-pkg.sh
@@ -1,7 +1,26 @@
 #! /bin/sh
 
+cd commandline
+
+case $BUILD_OS in
+  Windows)
+    mingw32-make USBLIBS=-lusb0
+    ZIPLIB=/mingw64/bin/libusb*.dll
+    EXE_EXT=.exe
+    ;;
+  macOS)
+    make USBFLAGS=$(PKG_CONFIG_PATH=$(brew --prefix libusb-compat)/lib/pkgconfig pkg-config --cflags libusb) \
+         USBLIBS="$(PKG_CONFIG_PATH=$(brew --prefix libusb-compat)/lib/pkgconfig pkg-config --libs libusb) -lusb"
+    ZIPLIB=$(find $(brew --prefix libusb-compat)/lib -name "*dylib" -type f -depth 1)
+    ;;
+  Linux)
+    make
+    ;;
+esac
+
+git clone https://github.com/digistump/avr-dummy
+g++ -o launcher${EXE_EXT} avr-dummy/avrdude-dummy.cpp
+
 ZIPFILE=micronucleus-$(uname -m)-$(uname -s).zip
 if [ -n "$ZIPLIB" ]; then eval "cp $ZIPLIB ."; ZIPLIB=$(basename "$ZIPLIB"); fi
 eval zip $ZIPFILE micronucleus${EXE_EXT} launcher${EXE_EXT} $ZIPLIB
-
-
