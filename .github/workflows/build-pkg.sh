0d2a0e110125e06027345416f4d8238fa3434b73
Thomas Quinot <thomas@quinot.org>
2021-01-23 23:24:58 +0100

    Fix DLL handling

--- .github/workflows/build-pkg.sh
+++ .github/workflows/build-pkg.sh
@@ -1,7 +1,7 @@
 #! /bin/sh
 
 ZIPFILE=micronucleus-$(uname -m)-$(uname -s).zip
-if [ -n "$ZIPLIB" ]; then cp "$ZIPLIB"; ZIPLIB=$(basename "$ZIPLIB"); fi
-zip $ZIPFILE micronucleus${EXE_EXT} launcher${EXE_EXT} $ZIPLIB
+if [ -n "$ZIPLIB" ]; then eval "cp $ZIPLIB ."; ZIPLIB=$(basename "$ZIPLIB"); fi
+eval zip $ZIPFILE micronucleus${EXE_EXT} launcher${EXE_EXT} $ZIPLIB
 
 
