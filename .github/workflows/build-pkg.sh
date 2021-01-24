052ad1d24864dc16f41291bf0580ddbc31311f88
Thomas Quinot <thomas@quinot.org>
2021-01-24 14:27:08 +0100

    Publish release artifacts

--- .github/workflows/build-pkg.sh
+++ .github/workflows/build-pkg.sh
@@ -24,3 +24,19 @@ g++ -o launcher${EXE_EXT} avr-dummy/avrdude-dummy.cpp
 ZIPFILE=micronucleus-$(uname -m)-$(uname -s).zip
 if [ -n "$ZIPLIB" ]; then eval "cp $ZIPLIB ."; ZIPLIB=$(basename "$ZIPLIB"); fi
 eval zip $ZIPFILE micronucleus${EXE_EXT} launcher${EXE_EXT} $ZIPLIB
+
+## Prepare release and artifact upload
+
+case $GITHUB_REF in
+    refs/tags/*)
+        RELEASE_TAG=$(basename $GITHUB_REF)
+        ;;
+    refs/heads/*)
+        RELEASE_BRANCH=$(basename $GITHUB_REF)-LATEST
+        ;;
+esac
+RELEASE_FILE=$(pwd)/$ZIPFILE
+for release_item in TAG BRANCH FILE
+do
+  eval echo "::set-output name=RELEASE_$release_item::\$RELEASE_$release_item"
+done
